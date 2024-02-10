dd211e557cb2b3bbdaf788d1fae2e96f862b4df6
BarNahmuka <104518127+BarNahmuka@users.noreply.github.com>
2024-02-11 00:36:05 +0200

    upload assigmnet

---
+++ app/src/main/java/com/example/tv/MyData.java
@@ -0,0 +1,22 @@
+package com.example.tv;
+
+public class MyData {
+
+    static String[] nameArray = {"Goku", "Vegeta", "Piccolo", "Gohan", "Future Trunks", "Krillin", "Frieza","Chi-Chi", "Raditz", "Master Roshi"};
+    static String[] descriptionArray = {"The central character in Dragon Ball, a Saiyan sent to Earth as an infant with a mission to destroy it.However, a head injury erased his memory of this destructive purpose, leading him to grow up as a protector of Earth and one of its greatest defenders.",
+            "initially a villain in Dragon Ball, evolves from a ruthless, prideful warrior into a complex character striving for redemption. His journey involves confronting his past, surpassing Goku, and finding peace on Earth.",
+            "A Namekian warrior initially introduced as a villain in Dragon Ball, undergoes a transformation after forming a bond with Goku's son, Gohan. His wise and cunning nature evolves, leading him to become one of Earth's greatest heroes.",
+            "The half-Saiyan son of Goku, is a scholarly individual who reluctantly engages in combat, only doing so to protect his loved ones. Despite his peaceful nature, Gohan showcases immense power when necessary.",
+            "A Saiyan-Earthling hybrid and son of Vegeta from an alternate future, is well-mannered, serious, and cautious. In his timeline, Future Androids wreak havoc, leading him to be trained by Gohan. He becomes a skilled fighter, swordsman, traveling back in time to prevent his future.",
+            "A protagonist in the Dragon Ball series.Despite often being overpowered by enemies, he remains courageous,faithful,and good-natured.Krillin provides comic relief with his short stature and baldness,though he retires from fighting to settle down with his family, marrying one of the Androids",
+            "The main antagonist of the Dragon Ball series, is ruthless emperor , inheriting power from his father. Known for his unparalleled cruelty and power, Frieza becomes a recurring villain, clashing with Goku and his allies multiple times, constantly growing stronger with each encounte.",
+            "Originally a princess,later marries Goku and becomes the mother of Gohan.Initially shy and fearful,she develops a tomboyish and fierce personality as she grows older,often displaying angry outbursts.However, her love for Goku and their sons is evident throughout the series.",
+            "known as Raditz the Runt, is a mid-class Saiyan warrior and the older brother of Goku. one of the few remaining full-blooded Saiyans after their homeworld's destruction. Raditz seeks out Goku to recruit him into the Frieza Force for conquering planets.",
+            "A master of martial arts. Despite his looks, he is highly respected and capable, serving as the master of several prominent characters including Goku and Krillin. He lives on his isolated island called Kame House and is known as the originator of the Kamehameha Wave."};
+
+    static Integer[] drawableArray = {R.drawable.a, R.drawable.vegita, R.drawable.pik,
+            R.drawable.go, R.drawable.trun, R.drawable.kri, R.drawable.ferjpeg,
+            R.drawable.chi, R.drawable.rad, R.drawable.mas};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+}
